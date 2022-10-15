11ee6a7f862260c4f8669ca2a3b3b7a1d00123dd
AharonSambol <75334174+AharonSambol@users.noreply.github.com>
2022-10-16 00:22:00 +0300

    added hashmap comprehension

--- test_macros/src/main.rs
+++ test_macros/src/main.rs
@@ -3,9 +3,9 @@ use list_comprehension_macro::comp;
 
 fn main() {
     let arr: Vec<u32> = (0..10).collect();
-    let new_arr = comp![x.pow(2) for x in &arr if x % 2 == 0];
+    let new_arr = comp![x.pow(2), x for x in &arr if x % 2 == 0];
     println!("{:?}", new_arr);
 
-    let a = base!(10100, 2);
-    println!("{}", a);
+    let num = base!(1A3C, 16); // = 6716
+    println!("{}", num);
 }
